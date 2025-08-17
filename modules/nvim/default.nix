4c0a9ffaddf6288f397bdb419acc0a8e232df83c
Dekoder-py <dekod3r.py@gmail.com>
2025-08-17 21:20:21 +1200

    update nvim options

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -9,9 +9,16 @@
     defaultEditor = true;
     
     extraLuaConfig = ''
-	vim.opt.tabstop = 2
-	vim.opt.shiftwidth = 2
-	vim.opt.expandtab = true
+      vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
+      vim.opt.shiftwidth = 2
+      vim.opt.expandtab = true
+
+      vim.opt.scrolloff = 10
+
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.opt.signcolumn = true
     '';
 
   };
