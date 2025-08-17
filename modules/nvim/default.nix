df474cf72ba1814f02183cf84a0e87c9e280461d
Dekoder-py <dekod3r.py@gmail.com>
2025-08-17 21:26:13 +1200

    update nvim to source options file

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -9,16 +9,8 @@
     defaultEditor = true;
     
     extraLuaConfig = ''
-      vim.opt.tabstop = 2
-      vim.opt.softtabstop = 2
-      vim.opt.shiftwidth = 2
-      vim.opt.expandtab = true
 
-      vim.opt.scrolloff = 10
-
-      vim.opt.number = true
-      vim.opt.relativenumber = true
-      vim.opt.signcolumn = "yes"
+      ${builtins.readFile ./options.lua}
     '';
 
   };
