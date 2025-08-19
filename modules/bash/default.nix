b47ee393816675be19b28b4ce04361a06a399941
Dekoder-py <dekod3r.py@gmail.com>
2025-08-19 21:52:09 +1200

    update all eza (ls alias)

--- modules/bash/default.nix
+++ modules/bash/default.nix
@@ -6,8 +6,8 @@
     shellAliases = {
       ff = "fastfetch";
       lg = "lazygit";
-      ls = "eza -1 --icons=always";
-      l = "eza -lh --icons=always --group-directories-first";
+      ls = "eza -1 --icons=always --sort=name --group-directories-first";
+      l = "eza -lh --icons=always --sort=name --group-directories-first";
       ll = "eza -lha --icons=always --sort=name --group-directories-first";
       ld = "eza -lhD --icons=always";
       lt = "eza --icons=always --tree";
