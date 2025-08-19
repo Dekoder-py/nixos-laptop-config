03e17e6073676b29971ed2bc48be480c5f479ad4
Dekoder-py <dekod3r.py@gmail.com>
2025-08-19 21:51:26 +1200

    fix l alias

--- modules/bash/default.nix
+++ modules/bash/default.nix
@@ -7,7 +7,7 @@
       ff = "fastfetch";
       lg = "lazygit";
       ls = "eza -1 --icons=always";
-      l = "eza -lh --icons=always";
+      l = "eza -lh --icons=always --group-directories-first";
       ll = "eza -lha --icons=always --sort=name --group-directories-first";
       ld = "eza -lhD --icons=always";
       lt = "eza --icons=always --tree";
